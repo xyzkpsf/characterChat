fb75a67514bc8f815146a1c1649170fbccd0610a
xyzkpsf <xyzkpsf@gmail.com>
2023-03-09 23:47:33 -0800

    add style component, working on chatview

--- client/src/Components/ChatView.js
+++ client/src/Components/ChatView.js
@@ -1,15 +1,53 @@
 import "react-chat-elements/dist/main.css";
-import { MessageBox } from "react-chat-elements";
+import { MessageList, Avatar, Input, Button } from "react-chat-elements";
 
+// take character object, and message list as input
 const ChatView = (props) => {
   return (
     <div>
-      <MessageBox
-        position={"left"}
-        type={"text"}
-        title={"Message Box Title"}
-        text="Here is a text type message box"
-      />
+      <div>
+        <Avatar
+          src="https://avatars.githubusercontent.com/u/80540635?v=4"
+          alt="avatar"
+          size="xlarge"
+          type="rounded"
+        />
+        ;
+      </div>
+      <div>
+        <MessageList
+          className="message-list"
+          lockable={true}
+          toBottomHeight={"100%"}
+          dataSource={[
+            {
+              position: "left",
+              type: "text",
+              title: "Kursat",
+              text: "Give me a message list example !",
+            },
+            {
+              position: "right",
+              type: "text",
+              title: "Emre",
+              text: "That's all.",
+            },
+          ]}
+        />
+      </div>
+      <div>
+        <Input placeholder="Type here..." multiline={true} />
+        <Button
+          text={"Send"}
+          onClick={() => alert("Sending...")}
+          title="Send"
+          // icon ={
+          //   float:'left',
+          //   size:15,
+          //   component:<IconExample/>
+          // }
+        />
+      </div>
     </div>
   );
 };
