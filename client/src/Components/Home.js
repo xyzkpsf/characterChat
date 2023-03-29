d5a9113c20139c0546d7d976b998bd49b836b39e
xyzkpsf <xyzkpsf@gmail.com>
2023-03-28 23:23:44 -0700

    dont pause and resume animation, change to animate profile pic when hover

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from 'styled-components';
 import { motion, useAnimationControls } from 'framer-motion';
 
@@ -68,48 +68,67 @@ const movingVariants = {
   })
 };
 
-//TODO: try to convert to variants and pause with useAnimationControls()
-const renderProfileSection = (controls) => {
-  let characters = clone(CHARACTERS);
-  shuffleArray(characters);
-  return characters.map((c, idx) => {
-    return (
-      <motion.div className={`row_1_${idx}_profile`} key={c.name} custom={idx} variants={movingVariants} animate={controls} onClick={() => console.log('click', c.name)}>
-        <ProfileWrapper>
-          <motion.div
-            // whileHover={{
-            //   rotateY: 180,
-            //   transition: {
-            //     type: 'smooth',
-            //     duration: 2
-            //   }
-            // }}
-            onMouseEnter={() => {
-              controls.stop();
-              const ele = document.getElementsByClassName(`row_1_${idx}_profile`);
-              console.log({ ele, first_child: ele[0], offSetLeft: ele[0].offsetLeft });
-            }}
-            onMouseLeave={() => {
-              controls.start('move');
-              // TODO: firgure out how to get curent x position
-              // TODO: create another variant that starts from the current position
-            }}
-          >
-            <img src={c.url} alt={c.name} />
-          </motion.div>
-          <span>{c.name}</span>
-        </ProfileWrapper>
-      </motion.div>
-    );
-  });
+const show = {
+  opacity: 1
+};
+
+const hide = {
+  opacity: 0
 };
 
 function Home(props) {
   const controls = useAnimationControls();
+  const [showProfile_0, setShowProfile_0] = useState(true);
+  const [showProfile_1, setShowProfile_1] = useState(true);
+  const [showProfile_2, setShowProfile_2] = useState(true);
+  const [showProfile_3, setShowProfile_3] = useState(true);
+  const [showProfile_4, setShowProfile_4] = useState(true);
+  const [showProfile_5, setShowProfile_5] = useState(true);
+  const [showProfile_6, setShowProfile_6] = useState(true);
+  const [showProfile_7, setShowProfile_7] = useState(true);
 
-  useEffect(() => {
-    controls.start('move');
-  });
+  const showProfiles = [
+    [showProfile_0, setShowProfile_0],
+    [showProfile_1, setShowProfile_1],
+    [showProfile_2, setShowProfile_2],
+    [showProfile_3, setShowProfile_3],
+    [showProfile_4, setShowProfile_4],
+    [showProfile_5, setShowProfile_5],
+    [showProfile_6, setShowProfile_6],
+    [showProfile_7, setShowProfile_7]
+  ];
+
+  // useEffect(() => {
+  //   controls.start('move');
+  // });
+
+  //TODO: try to convert to variants and pause with useAnimationControls()
+  const renderProfileSection = (controls) => {
+    let characters = clone(CHARACTERS);
+    shuffleArray(characters);
+    return characters.map((c, idx) => {
+      return (
+        <motion.div
+          key={c.name}
+          custom={idx}
+          variants={movingVariants}
+          animate={'move'}
+          // onMouseEnter={() => {
+          //   showProfiles[idx][1](false);
+          // }}
+          // onMouseLeave={() => {
+          //   showProfiles[idx][1](true);
+          // }}
+          onClick={() => console.log('click', c.name)}
+        >
+          <ProfileWrapper>
+            <motion.img key={c.url} src={c.url} />
+            <span>{c.name}</span>
+          </ProfileWrapper>
+        </motion.div>
+      );
+    });
+  };
 
   return (
     <HomeWrapper>
